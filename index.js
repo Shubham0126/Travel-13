8f6ad00ed21015ce2a7f7f32fe0bdea9246b9a50
Shubham Pal <115461478+Shubham0126@users.noreply.github.com>
2023-06-27 18:58:42 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+
+$(window).scroll(function(){
+  if ($(this).scrollTop() > 100) {
+    $('nav').addClass('bg-dark')
+    $('nav').addClass('fixed-top')
+  } else {
+      
+      $('nav').removeClass('bg-dark')
+  }
+});
+
+
+
+const subs_cont_elm = document.getElementById("subs-cont");
+const btn_sub_close = document.getElementById("subs-close");
+
+btn_sub_close.addEventListener("click", () => {
+  subs_cont_elm.classList.remove("show");
+
+  /* Code To Show Subscribe Modale After 5 Seconds */
+  setTimeout(() => {
+    subs_cont_elm.classList.add("show");
+  }, 1000 * 5);
+});
+
 $(".place-carousel").owlCarousel({
     autoplay: true,
     smartSpeed: 1500,
